4385248da89fe8528c816b601d9af003f631e4af
zhangpeihua <zhangpeihua@bytedance.com>
2022-05-30 15:16:14 +0800

    feat: add test cases for json utils

---
+++ common/common_vestack_callback_test.go
@@ -0,0 +1,70 @@
+package common
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSortAndStartTransJson1(t *testing.T) {
+	req := map[string]interface{}{
+		"Filter.ClusterId": "12345",
+	}
+	target := map[string]interface{}{
+		"Filter": map[string]interface{}{
+			"ClusterId": "12345",
+		},
+	}
+	resp := sortAndStartTransJson(req)
+	assert.Equal(t, resp, target)
+}
+
+func TestSortAndStartTransJson2(t *testing.T) {
+	req := sortAndStartTransJson(map[string]interface{}{
+		"Filter.Ids.1": "id123",
+		"Filter.Ids.2": "id456",
+	})
+	target := map[string]interface{}{
+		"Filter": map[string]interface{}{
+			"Ids": []interface{}{"id123", "id456"},
+		},
+	}
+	resp := sortAndStartTransJson(req)
+	assert.Equal(t, resp, target)
+}
+
+func TestSortAndStartTransJson3(t *testing.T) {
+	req := sortAndStartTransJson(map[string]interface{}{
+		"Filter.ClusterId": "12345",
+		"Filter.Ids.1":     "id123",
+		"Filter.Ids.2":     "id456",
+
+		"Filter.Nets.1.Subnet": "subnet1",
+		"Filter.Nets.2.Subnet": "subnet2",
+		"Filter.Nets.3.Subnet": "subnet3",
+	})
+	target := map[string]interface{}{
+		"Filter": map[string]interface{}{
+			"ClusterId": "12345",
+			"Ids":       []interface{}{"id123", "id456"},
+			"Nets": []interface{}{
+				map[string]interface{}{
+					"Subnet": "subnet1",
+				},
+				map[string]interface{}{
+					"Subnet": "subnet2",
+				},
+				map[string]interface{}{
+					"Subnet": "subnet3",
+				},
+			},
+		},
+	}
+	resp := sortAndStartTransJson(req)
+	assert.Equal(t, resp, target)
+
+	str := `{"Filter":{"ClusterId":"12345","Ids":["id123","id456"],"Nets":[{"Subnet":"subnet1"},{"Subnet":"subnet2"},{"Subnet":"subnet3"}]}}`
+	bytes, _ := json.Marshal(resp)
+	assert.Equal(t, str, string(bytes))
+}
