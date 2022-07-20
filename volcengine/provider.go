841282959dd34398be71faad238cdedc3fda8316
songzheng <songzheng.sz@bytedance.com>
2022-07-20 16:47:57 +0800

    feat: support account privilege resource

--- volcengine/provider.go
+++ volcengine/provider.go
@@ -25,6 +25,7 @@ import (
 	"github.com/volcengine/terraform-provider-volcengine/volcengine/nat/nat_gateway"
 	"github.com/volcengine/terraform-provider-volcengine/volcengine/nat/snat_entry"
 	"github.com/volcengine/terraform-provider-volcengine/volcengine/rds/rds_account"
+	"github.com/volcengine/terraform-provider-volcengine/volcengine/rds/rds_account_privilege"
 	"github.com/volcengine/terraform-provider-volcengine/volcengine/rds/rds_database"
 	"github.com/volcengine/terraform-provider-volcengine/volcengine/rds/rds_instance"
 	"github.com/volcengine/terraform-provider-volcengine/volcengine/rds/rds_ip_list"
@@ -174,10 +175,11 @@ func Provider() terraform.ResourceProvider {
 			"volcengine_vke_node_pool": node_pool.ResourceVolcengineNodePool(),
 
 			// ================ RDS V1 ==============
-			"volcengine_rds_instance": rds_instance.ResourceVolcengineRdsInstance(),
-			"volcengine_rds_database": rds_database.ResourceVolcengineRdsDatabase(),
-			"volcengine_rds_account":  rds_account.ResourceVolcengineRdsAccount(),
-			"volcengine_rds_ip_list":  rds_ip_list.ResourceVolcengineRdsIpList(),
+			"volcengine_rds_instance":          rds_instance.ResourceVolcengineRdsInstance(),
+			"volcengine_rds_database":          rds_database.ResourceVolcengineRdsDatabase(),
+			"volcengine_rds_account":           rds_account.ResourceVolcengineRdsAccount(),
+			"volcengine_rds_ip_list":           rds_ip_list.ResourceVolcengineRdsIpList(),
+			"volcengine_rds_account_privilege": rds_account_privilege.ResourceVolcengineRdsAccountPrivilege(),
 		},
 		ConfigureFunc: ProviderConfigure,
 	}
