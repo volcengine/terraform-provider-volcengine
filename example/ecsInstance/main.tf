166e15a705ae3c75fd72473ded42c196f17f2865
xuyaming <xuyaming@bytedance.com>
2023-02-22 19:37:50 +0800

    bugfix: ecs postpaid-> prepaid

--- example/ecsInstance/main.tf
+++ example/ecsInstance/main.tf
@@ -6,7 +6,7 @@ resource "volcengine_vpc" "foo" {
 resource "volcengine_subnet" "foo1" {
   subnet_name = "subnet-test-1"
   cidr_block = "172.16.1.0/24"
-  zone_id = "cn-shanghai-a"
+  zone_id = "cn-beijing-a"
   vpc_id = volcengine_vpc.foo.id
 }
 
@@ -16,20 +16,18 @@ resource "volcengine_security_group" "foo1" {
 }
 
 resource "volcengine_ecs_instance" "default" {
-  image_id = "image-ybm3r19o545id8bxuanl"
-  instance_type = "ecs.g2i.large"
-  instance_name = "xym-tf-test-3"
+  image_id = "image-aagd56zrw2jtdro3bnrl"
+  instance_type = "ecs.g1.large"
+  instance_name = "xym-tf-test-2"
   description = "xym-tf-test-desc-1"
   password = "93f0cb0614Aab12"
-  instance_charge_type = "PrePaid"
-  period = 1
-#  auto_renew =false
-  system_volume_type = "ESSD_PL0"
+  instance_charge_type = "PostPaid"
+  system_volume_type = "PTSSD"
   system_volume_size = 60
   subnet_id = volcengine_subnet.foo1.id
   security_group_ids = [volcengine_security_group.foo1.id]
   data_volumes {
-    volume_type = "ESSD_PL0"
+    volume_type = "PTSSD"
     size = 100
     delete_with_instance = true
   }
