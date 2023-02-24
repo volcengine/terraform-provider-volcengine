7e108356129cad8e6bc3e4a542682019beaebc9b
lixiang <lixiang.byte@bytedance.com>
2023-02-24 10:51:42 +0800

    fix:add update timeout for rds mysql account&database.

--- volcengine/rds_mysql/rds_mysql_database/resource_volcengine_rds_database.go
+++ volcengine/rds_mysql/rds_mysql_database/resource_volcengine_rds_database.go
@@ -29,6 +29,7 @@ func ResourceVolcengineRdsMysqlDatabase() *schema.Resource {
 		},
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
 			Delete: schema.DefaultTimeout(30 * time.Minute),
 		},
 		Schema: map[string]*schema.Schema{
