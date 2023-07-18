39315c9df1ac740e965a9a27d40e70513b1c536d
张哲铭 <zhangzheming.zzmher@bytedance.com>
2023-07-18 15:10:32 +0800

    feat: add network acl datasource test

--- volcengine/vpc/network_acl/data_source_volcengine_network_acls_test.go
+++ volcengine/vpc/network_acl/data_source_volcengine_network_acls_test.go
@@ -9,11 +9,37 @@ import (
 )
 
 const testAccNetworkAclConfig = `
-	
+	data "volcengine_zones" "foo"{
+	}
+
+	resource "volcengine_vpc" "foo" {
+	  vpc_name   = "acc-test-vpc"
+	  cidr_block = "172.16.0.0/16"
+	}
+
+	resource "volcengine_network_acl" "foo" {
+	  vpc_id = "${volcengine_vpc.foo.id}"
+	  network_acl_name = "acc-test-acl"
+	  ingress_acl_entries {
+		network_acl_entry_name = "acc-ingress1"
+		policy = "accept"
+		protocol = "all"
+		source_cidr_ip = "192.168.0.0/24"
+	  }
+	  egress_acl_entries {
+		network_acl_entry_name = "acc-egress2"
+		policy = "accept"
+		protocol = "all"
+		destination_cidr_ip = "192.168.0.0/16"
+	  }
+	}
+	data "volcengine_network_acls" "foo"{
+  	network_acl_name = "acc-test-acl"
+	}
 `
 
-func TestAccVolcengineIpv6GatewayDataSource_Basic(t *testing.T) {
-	resourceName := "data.volcengine_vpc_network_acls.foo"
+func TestAccVolcengineNetworkAclDataSource_Basic(t *testing.T) {
+	resourceName := "data.volcengine_network_acls.foo"
 	acc := &volcengine.AccTestResource{
 		ResourceId: resourceName,
 		Svc:        &network_acl.VolcengineNetworkAclService{},
