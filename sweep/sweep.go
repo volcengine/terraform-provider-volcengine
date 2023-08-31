aada3248fbe1fe040be79afd1cd3bc441a7985c8
maoshuai.17 <maoshuai.17@bytedance.com>
2023-08-31 15:46:10 +0800

    feat: opt sweep client

--- sweep/sweep.go
+++ sweep/sweep.go
@@ -3,6 +3,8 @@ package sweep
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 
 	ve "github.com/volcengine/terraform-provider-volcengine/common"
@@ -10,26 +12,56 @@ import (
 )
 
 func SharedClientForRegionWithResourceId(region string) (interface{}, error) {
-	var accessKey string
+	var (
+		accessKey  string
+		secretKey  string
+		endpoint   string
+		disableSSL bool
+	)
+
 	if accessKey = os.Getenv("VOLCENGINE_ACCESS_KEY"); accessKey == "" {
 		return nil, fmt.Errorf("%s can not be empty", "VOLCENGINE_ACCESS_KEY")
 	}
-
-	var secretKey string
 	if secretKey = os.Getenv("VOLCENGINE_SECRET_KEY"); secretKey == "" {
 		return nil, fmt.Errorf("%s can not be empty", "VOLCENGINE_SECRET_KEY")
 	}
-
-	var endpoint string
 	if endpoint = os.Getenv("VOLCENGINE_ENDPOINT"); endpoint == "" {
 		return nil, fmt.Errorf("%s can not be empty", "VOLCENGINE_ENDPOINT")
 	}
+	disableSSL, _ = strconv.ParseBool(os.Getenv("VOLCENGINE_DISABLE_SSL"))
 
 	config := ve.Config{
-		AccessKey: accessKey,
-		SecretKey: secretKey,
-		Region:    region,
-		Endpoint:  endpoint,
+		AccessKey:         accessKey,
+		SecretKey:         secretKey,
+		Region:            region,
+		Endpoint:          endpoint,
+		DisableSSL:        disableSSL,
+		SessionToken:      os.Getenv("VOLCENGINE_SESSION_TOKEN"),
+		ProxyUrl:          os.Getenv("VOLCENGINE_PROXY_URL"),
+		CustomerHeaders:   map[string]string{},
+		CustomerEndpoints: defaultCustomerEndPoints(),
+	}
+
+	headers := os.Getenv("VOLCENGINE_CUSTOMER_HEADERS")
+	if headers != "" {
+		hs1 := strings.Split(headers, ",")
+		for _, hh := range hs1 {
+			hs2 := strings.Split(hh, ":")
+			if len(hs2) == 2 {
+				config.CustomerHeaders[hs2[0]] = hs2[1]
+			}
+		}
+	}
+
+	endpoints := os.Getenv("VOLCENGINE_CUSTOMER_ENDPOINTS")
+	if endpoints != "" {
+		ends := strings.Split(endpoints, ",")
+		for _, end := range ends {
+			point := strings.Split(end, ":")
+			if len(point) == 2 {
+				config.CustomerEndpoints[point[0]] = point[1]
+			}
+		}
 	}
 
 	client, err := config.Client()
@@ -38,6 +70,12 @@ func SharedClientForRegionWithResourceId(region string) (interface{}, error) {
 	}
 
 	return client, nil
+}
+
+func defaultCustomerEndPoints() map[string]string {
+	return map[string]string{
+		"veenedge": "veenedge.volcengineapi.com",
+	}
 }
 
 type SweeperInstance interface {
