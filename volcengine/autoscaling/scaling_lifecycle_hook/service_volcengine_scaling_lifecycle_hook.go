29897536c11f52824cb172a0aaaab0f488e3e274
张哲铭 <zhangzheming.zzmher@bytedance.com>
2023-11-03 10:54:17 +0800

    fix: opt as lifecycle command modify

--- volcengine/autoscaling/scaling_lifecycle_hook/service_volcengine_scaling_lifecycle_hook.go
+++ volcengine/autoscaling/scaling_lifecycle_hook/service_volcengine_scaling_lifecycle_hook.go
@@ -178,7 +178,12 @@ func (s *VolcengineScalingLifecycleHookService) ModifyResource(resourceData *sch
 				}
 				(*call.SdkParam)["LifecycleHookId"] = ids[1]
 				if d.HasChange("lifecycle_command") {
-					(*call.SdkParam)["LifecycleCommand.CommandId"] = d.Get("lifecycle_command.0.command_id")
+					commandId, ok := d.GetOk("lifecycle_command.0.command_id")
+					if ok {
+						(*call.SdkParam)["LifecycleCommand.CommandId"] = commandId
+					} else {
+						(*call.SdkParam)["LifecycleCommand.CommandId"] = ""
+					}
 					params, ok := d.GetOk("lifecycle_command.0.parameters")
 					if ok {
 						(*call.SdkParam)["LifecycleCommand.Parameters"] = params
