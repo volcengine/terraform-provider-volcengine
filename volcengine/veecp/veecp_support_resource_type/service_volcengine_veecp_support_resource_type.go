5a528c96dbe8344feef63810f8cc2674edcaddcc
张哲铭 <zhangzheming.zzmher@bytedance.com>
2025-02-19 16:19:09 +0800

    feat: 支持veecp集群插件和资源类型查询

---
+++ volcengine/veecp/veecp_support_resource_type/service_volcengine_veecp_support_resource_type.go
@@ -0,0 +1,128 @@
+package veecp_support_resource_type
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	ve "github.com/volcengine/terraform-provider-volcengine/common"
+	"github.com/volcengine/terraform-provider-volcengine/logger"
+)
+
+type VolcengineVeecpSupportResourceTypeService struct {
+	Client     *ve.SdkClient
+	Dispatcher *ve.Dispatcher
+}
+
+func NewVeecpSupportResourceTypeService(c *ve.SdkClient) *VolcengineVeecpSupportResourceTypeService {
+	return &VolcengineVeecpSupportResourceTypeService{
+		Client:     c,
+		Dispatcher: &ve.Dispatcher{},
+	}
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) GetClient() *ve.SdkClient {
+	return s.Client
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) ReadResources(m map[string]interface{}) (data []interface{}, err error) {
+	var (
+		resp    *map[string]interface{}
+		results interface{}
+		ok      bool
+	)
+	return ve.WithPageNumberQuery(m, "PageSize", "PageNumber", 100, 1, func(condition map[string]interface{}) ([]interface{}, error) {
+		action := "ListSupportedResourceTypes"
+
+		bytes, _ := json.Marshal(condition)
+		logger.Debug(logger.ReqFormat, action, string(bytes))
+		if condition == nil {
+			resp, err = s.Client.UniversalClient.DoCall(getUniversalInfo(action), nil)
+			if err != nil {
+				return data, err
+			}
+		} else {
+			resp, err = s.Client.UniversalClient.DoCall(getUniversalInfo(action), &condition)
+			if err != nil {
+				return data, err
+			}
+		}
+		respBytes, _ := json.Marshal(resp)
+		logger.Debug(logger.RespFormat, action, condition, string(respBytes))
+
+		results, err = ve.ObtainSdkValue("Result.Items", *resp)
+		if err != nil {
+			return data, err
+		}
+		if results == nil {
+			results = []interface{}{}
+		}
+		if data, ok = results.([]interface{}); !ok {
+			return data, errors.New("Result.Items is not Slice")
+		}
+		return data, err
+	})
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) ReadResource(resourceData *schema.ResourceData, id string) (data map[string]interface{}, err error) {
+	return data, err
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) RefreshResourceState(resourceData *schema.ResourceData, target []string, timeout time.Duration, id string) *resource.StateChangeConf {
+	return &resource.StateChangeConf{}
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) CreateResource(resourceData *schema.ResourceData, resource *schema.Resource) []ve.Callback {
+	callback := ve.Callback{}
+	return []ve.Callback{callback}
+}
+
+func (VolcengineVeecpSupportResourceTypeService) WithResourceResponseHandlers(d map[string]interface{}) []ve.ResourceResponseHandler {
+	handler := func() (map[string]interface{}, map[string]ve.ResponseConvert, error) {
+		return d, nil, nil
+	}
+	return []ve.ResourceResponseHandler{handler}
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) ModifyResource(resourceData *schema.ResourceData, resource *schema.Resource) []ve.Callback {
+	callback := ve.Callback{}
+	return []ve.Callback{callback}
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) RemoveResource(resourceData *schema.ResourceData, r *schema.Resource) []ve.Callback {
+	callback := ve.Callback{}
+	return []ve.Callback{callback}
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) DatasourceResources(*schema.ResourceData, *schema.Resource) ve.DataSourceInfo {
+	return ve.DataSourceInfo{
+		RequestConverts: map[string]ve.RequestConvert{
+			"zone_ids": {
+				TargetField: "Filter.ZoneIds",
+				ConvertType: ve.ConvertJsonArray,
+			},
+			"resource_types": {
+				TargetField: "Filter.ResourceTypes",
+				ConvertType: ve.ConvertJsonArray,
+			},
+		},
+		ContentType:  ve.ContentTypeJson,
+		CollectField: "resources",
+	}
+}
+
+func (s *VolcengineVeecpSupportResourceTypeService) ReadResourceId(id string) string {
+	return id
+}
+
+func getUniversalInfo(actionName string) ve.UniversalInfo {
+	return ve.UniversalInfo{
+		ServiceName: "veecp_openapi",
+		Version:     "2021-03-03",
+		HttpMethod:  ve.POST,
+		ContentType: ve.ApplicationJSON,
+		Action:      actionName,
+	}
+}
