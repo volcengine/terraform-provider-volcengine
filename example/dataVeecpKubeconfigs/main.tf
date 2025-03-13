9a2a31463ba724e3ee20dc50c1225c082893de4f
张哲铭 <zhangzheming.zzmher@bytedance.com>
2025-03-13 16:24:51 +0800

    feat: 支持veecp kubeconfig

---
+++ example/dataVeecpKubeconfigs/main.tf
@@ -0,0 +1,8 @@
+data "volcengine_veecp_kubeconfigs" "foo"{
+    cluster_ids = []
+    ids = []
+    page_number = 1
+    page_size = 1
+    role_ids = []
+    types = []
+}
---
+++ example/veecpKubeconfig/main.tf
@@ -0,0 +1,4 @@
+resource "volcengine_veecp_kubeconfig" "foo" {
+    cluster_id = ""
+    type = ""
+}
